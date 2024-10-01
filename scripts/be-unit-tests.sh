4ad27412c3b0ccd4deaccb34ee4b9338d97ec88c
Eric John <156272509+erickqjohn@users.noreply.github.com>
2024-10-01 14:10:56 -0400

    Update be-unit-tests.sh

--- scripts/be-unit-tests.sh
+++ scripts/be-unit-tests.sh
@@ -4,6 +4,9 @@ set -o pipefail
 
 repo_dir_name=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")
 
+echo "$PWD"
+echo ${repo_dir_name} 
+
 pushd .
 cd ${repo_dir_name}/server
 npm install
