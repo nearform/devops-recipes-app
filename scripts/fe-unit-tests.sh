010bd27a1810918fc78fa4d52841609208d64bfa
Eric John <156272509+erickqjohn@users.noreply.github.com>
2024-10-01 14:49:17 -0400

    Update fe-unit-tests.sh

--- scripts/fe-unit-tests.sh
+++ scripts/fe-unit-tests.sh
@@ -2,14 +2,7 @@
 set -eu
 set -o pipefail
 
-# repo_dir_name=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")
-
-# pushd .
-# cd ${repo_dir_name}/client
 echo "$PWD"
 ls
 ls -l
-cd ./client
-npm install
-CI=true npm run test
-popd
+
