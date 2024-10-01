a28cecabb93bc00fa4aee67b81891a431c58f674
Eric John <156272509+erickqjohn@users.noreply.github.com>
2024-10-01 14:27:08 -0400

    Update be-unit-tests.sh

--- scripts/be-unit-tests.sh
+++ scripts/be-unit-tests.sh
@@ -2,8 +2,9 @@
 # set -eu
 # set -o pipefail
 
-repo_dir_name=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")
-
+echo repo_dir_name=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")
+ls
+ls -l
 echo "$PWD"
 echo ${repo_dir_name} 
 
